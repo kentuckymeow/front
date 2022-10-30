17b8db1cdb1a78c7d7124bf95f5c7c7a0857b3ec
ArseniKhatsuk <khatsuk007@gmail.com>
2022-10-30 22:03:31 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+    const handleClick = () => alert('say gav');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type = 'text' placeholder='login'/>
+        <input type = 'password' placeholder='password' />
+
+      <button onClick = {handleClick} >
+           click me
+      </button>
     </div>
   );
 }
