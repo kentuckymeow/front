b7c19cce0a0e345cb638657e2aff433a83e5ab4b
ArseniKhatsuk <khatsuk007@gmail.com>
2022-11-08 13:42:15 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 
 import './App.css';
+import React from 'react';
 
-const App = () => {
 
-    const handleClick = () => alert('say gav');
-  return (
-    <div className="App">
-        <input type = 'text' placeholder='login'/>
-        <input type = 'password' placeholder='password' />
+    const initialFormData = Object.freeze({
+        login: "",
+        password: ""
+    });
+    const App = () => {
+        const [formData, updateFormData] = React.useState(initialFormData);
 
-      <button onClick = {handleClick} >
-           click me
-      </button>
-    </div>
-  );
+        const handleChange = (e) => {
+            updateFormData({
+                ...formData,
+
+                // Trimming any whitespace
+                [e.target.name]: e.target.value.trim()
+            });
+        };
+
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            console.log(formData);
+            // ... submit to API or something
+        };
+
+        return (
+            <div>
+                <label>
+                    Login
+                    <input name="login" onChange={handleChange} />
+                </label>
+                <br />
+                <label>
+                    Password
+                    <input name="password" onChange={handleChange} type={"password"} />
+                </label>
+                <br />
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+        );
+
 }
-
 export default App;
